e516657b2a3ee397fcf046999475f389a22be161
denv3rr <denver.clark@g.austincc.edu>
2024-09-16 17:20:30 -0500

    initial files + gitignore changes

---
+++ include/TrafficSimulation.h
@@ -0,0 +1,11 @@
+// include/TrafficSimulation.h
+#ifndef TRAFFICSIMULATION_H
+#define TRAFFICSIMULATION_H
+
+class TrafficSimulation
+{
+public:
+    void run();
+};
+
+#endif // TRAFFICSIMULATION_H
---
+++ src/TrafficSimulation.cpp
@@ -0,0 +1,10 @@
+// src/TrafficSimulation.cpp
+#include "TrafficSimulation.h"
+#include <iostream>
+
+void TrafficSimulation::run()
+{
+    std::cout << "Running Traffic Simulation" << std::endl;
+
+    // simulation logic here
+}
