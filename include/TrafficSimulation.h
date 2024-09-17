0d58d5c2ba8734c03c157a6648816c0751a33a5f
denv3rr <denver.clark@g.austincc.edu>
2024-09-17 00:07:21 -0500

    starting update and render functions for vehicles

--- include/TrafficSimulation.h
+++ include/TrafficSimulation.h
@@ -2,10 +2,19 @@
 #ifndef TRAFFICSIMULATION_H
 #define TRAFFICSIMULATION_H
 
+#include <vector>
+#include "Vehicle.h"
+
 class TrafficSimulation
 {
 public:
+    TrafficSimulation();
+    void update(float deltaTime);
+    void render();
     void run();
+
+private:
+    std::vector<Vehicle> vehicles;
 };
 
 #endif // TRAFFICSIMULATION_H
--- src/TrafficSimulation.cpp
+++ src/TrafficSimulation.cpp
@@ -1,10 +1,45 @@
 // src/TrafficSimulation.cpp
+/******************************************
+ * Handles traffic simulation logic
+ *******************************************/
 #include "TrafficSimulation.h"
 #include <iostream>
 
 void TrafficSimulation::run()
 {
-    std::cout << "Running Traffic Simulation" << std::endl;
+    /******************************************
+     * Runs traffic simulation logic
+     *******************************************/
+    std::cout << "Running simulation..." << std::endl;
 
-    // simulation logic here
+    // Initialize vehicles
+    vehicles.push_back(Vehicle(100.0f, 100.0f));
+
+    // ADD VEHICLES under here ****************
+}
+
+/******************************************
+ * Traffic Logic
+ *******************************************/
+
+void TrafficSimulation::update(float deltaTime)
+{
+    /******************************************
+     * Traffic update
+     *******************************************/
+    for (auto &vehicle : vehicles)
+    {
+        vehicle.update(deltaTime);
+    }
+}
+
+void TrafficSimulation::render()
+{
+    /******************************************
+     * Traffic render
+     *******************************************/
+    for (auto &vehicle : vehicles)
+    {
+        vehicle.render();
+    }
 }
