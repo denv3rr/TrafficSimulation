00c8f685f8725b3c3dd9a6b9f1eaa6e6a2c0a767
denv3rr <denver.clark@g.austincc.edu>
2024-09-16 23:48:55 -0500

    first function to render vehicle into a simple rectange first

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -11,3 +11,16 @@ void Vehicle::update(float deltaTime)
      *******************************************/
     x += speed * deltaTime;
 }
+
+void Vehicle::render()
+{
+    /******************************************
+     * Renders the vehicle as a simple rectangle first
+     *******************************************/
+    glBegin(GL_QUADS);
+    glVertex2f(x - 5.0f, y - 5.0f);
+    glVertex2f(x + 5.0f, y - 5.0f);
+    glVertex2f(x + 5.0f, y + 5.0f);
+    glVertex2f(x - 5.0f, y + 5.0f);
+    glEnd();
+}
