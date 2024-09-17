ed377aca48c5321b67a068d83221427467f3a699
denv3rr <denver.clark@g.austincc.edu>
2024-09-16 23:19:10 -0500

    added error return for failure to load GLEW. + comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
+// main.cpp - main project entrypoint
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 int main()
 {
+    /******************************************
+     * Loads GLFW, GLFW window, GLEW, then renders.
+     *******************************************/
+
+    // Error catch for inability to load GLFW window
     if (!glfwInit())
     {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
+    // Error catch for inability to load GLFW window
     GLFWwindow *window = glfwCreateWindow(800, 600, "Traffic Simulation", nullptr, nullptr);
     if (!window)
     {
@@ -20,11 +27,19 @@ int main()
 
     glfwMakeContextCurrent(window);
 
+    // Error catch for inability to load GLEW
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // Scene rendering
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Render your scene here
+        // *** RENDER SCENE ***
 
         glfwSwapBuffers(window);
         glfwPollEvents();
